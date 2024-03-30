93c908f08ca49a2ce8ff3e029a2b88d0362b0801
Tom Bell <tomb@tomb.io>
2024-03-30 02:04:15 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,19 +1,18 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.10
 
 import PackageDescription
 
 let package = Package(
-    name: "alicia",
-    platforms: [
-        .macOS(.v12),
-    ],
-    products: [
-        .library(name: "Alicia", targets: ["Alicia"]),
-    ],
-    dependencies: [
-    ],
-    targets: [
-        .target(name: "Alicia", dependencies: []),
-        .testTarget(name: "AliciaTests", dependencies: ["Alicia"]),
-    ]
+  name: "alicia",
+  platforms: [
+    .macOS(.v12)
+  ],
+  products: [
+    .library(name: "Alicia", targets: ["Alicia"])
+  ],
+  dependencies: [],
+  targets: [
+    .target(name: "Alicia", dependencies: []),
+    .testTarget(name: "AliciaTests", dependencies: ["Alicia"]),
+  ]
 )
